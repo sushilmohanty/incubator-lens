51e4d85644d14a7d15f8fe076823a3585e647713
sushilmohanty <sushil.k.mohanty@gmail.com>
2017-03-27 16:44:38 +0530

    LENS-1403

--- lens-cube/src/main/java/org/apache/lens/cube/parse/StorageCandidate.java
+++ lens-cube/src/main/java/org/apache/lens/cube/parse/StorageCandidate.java
@@ -247,6 +247,8 @@ public class StorageCandidate implements Candidate, CandidateTable {
     if (this == cubeql.getPickedCandidate()) {
       CandidateUtil.updateFinalAlias(queryAst.getSelectAST(), cubeql);
       updateOrderByWithFinalAlias(queryAst.getOrderByAST(), queryAst.getSelectAST());
+    } else {
+      queryAst.setHavingAST(null);
     }
     return CandidateUtil
       .buildHQLString(queryAst.getSelectString(), fromString, whereString, queryAst.getGroupByString(),
